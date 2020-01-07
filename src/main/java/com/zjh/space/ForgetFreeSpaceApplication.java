90ed3a71d4ead95d5992ef5d7460e89dda9e021f
heanhan <1763124707@qq.com>
2020-01-07 15:58:16 +0800

    首次提交个人随记，使用springboot 2.2.11、swagger 、mybatis、bootstrap 搭建个人的随记网站。

---
+++ src/main/java/com/zjh/space/ForgetFreeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetFreeSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgetFreeSpaceApplication.class, args);
+    }
+
+}
